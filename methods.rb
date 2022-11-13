66dd7fc03091d03000fa0cd96bc624d65515de81
olasrum <mursalo@gmail.com>
2022-11-13 14:12:17 +1000

    Done with assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,37 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+    "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet(name)
+    puts "Hello, #{name}!"
+    "Hello, #{name}!"
+end
+
+ greet("Naureen")
+
+def greet_with_default(name = "programmer") 
+   puts "Hello, #{name}!"
+   "Hello, #{name}!"
+end
+
+greet_with_default
+greet_with_default("Sunny")
+
+def add(num1, num2) 
+    num1 + num2
+end
+
+sum = add(1, 2)
+puts sum
+
+def halve(num) 
+    return nil unless num.class == Integer
+    num / 2
+end
+
+result = halve(4)
+puts result
+  
